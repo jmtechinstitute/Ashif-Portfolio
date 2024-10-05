eca23edbf1b9c8837bc7ec3c1a3b80bc0b2bec58
Mohamed Yazeen <166999856+jmtechinstitute@users.noreply.github.com>
2024-10-05 21:46:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings : ["JM TECH Mentor","FSWD Professer","Full Stack Developer","B.A.Enlish Student"],
+    typeSpeed:30,
+    backSpeed:50,
+    backdelay:1000,
+    loop:true
+})
